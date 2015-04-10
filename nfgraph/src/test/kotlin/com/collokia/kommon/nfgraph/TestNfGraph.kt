44dc4f904a348a90d6acc5063c418fd58f58b311
jaysonminard <jayson.minard@gmail.com>
2015-04-10 00:54:44 -0300

    refactoring of nfgraph code

--- nfgraph/src/test/kotlin/com/collokia/kommon/nfgraph/TestNfGraph.kt
+++ nfgraph/src/test/kotlin/com/collokia/kommon/nfgraph/TestNfGraph.kt
@@ -9,6 +9,8 @@ import kotlin.test.assertTrue
 import com.collokia.kommon.testing
 import com.collokia.kommon.nfgraph.MyNodes.*
 import com.collokia.kommon.nfgraph.MyRelations.*
+import com.collokia.kommon.nfgraph.internal.NodeAndId
+import com.collokia.kommon.nfgraph.internal.RelationStructure
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 import kotlin.test.fail
@@ -33,13 +35,22 @@ enum class MyRelations {
 
 public class TestNfGraph {
 
+
+    private fun Set<NodeAndId<MyNodes>>.hasOnly(testFor: Set<NodeAndId<MyNodes>>): Boolean {
+        return (this.size() == testFor.size() && this.containsAll(testFor))
+    }
+
     [Test] fun basicGraphBuilding() {
         val schema = defineGraphSchema<MyNodes, MyRelations>(RelationStructure.COMPACT) {
-            from(Movie).manyEdges(Starring).target(Actor).mirrorManyEdges(StarredIn)
-            from(Movie).oneEdge(DirectedBy).target(Director).mirrorManyEdges(Directed)
-            from(Movie).manyEdges(WonAward).target(Award).mirrorManyEdges(AwardWinner)
-            from(Actor).manyEdges(WonAward).target(Award).mirrorManyEdges(AwardWinner)  // creates award with two identical relations but to different targets
-            from(Director).manyEdges(WonAward).target(Award).mirrorManyEdges(AwardWinner)
+            // maybe syntax like
+            //    Movie[Starring]..Actor[StarredIn]
+            // need to be able to show many versus single in other syntax
+
+            from(Movie).connectEdges(Starring).target(Actor).autoMirrorEdges(StarredIn)
+            from(Movie).connectOneEdge(DirectedBy).target(Director).autoMirrorEdges(Directed)
+            from(Movie).connectEdges(WonAward).target(Award).hashed().autoMirrorEdges(AwardWinner).compact()
+            from(Actor).connectEdges(WonAward).target(Award).autoMirrorEdges(AwardWinner)  // we support duplicate relations A->R->B and A->R->C by creating A->R.B->B and A->R.C->C under the covers
+            from(Director).connectEdges(WonAward).target(Award).autoMirrorEdges(AwardWinner)
 
             // TODO: check dupes overwriting with same settings
 
@@ -68,34 +79,27 @@ public class TestNfGraph {
             connect(Movie["Indiana Jones"], WonAward, Award["People's Choice Award"])
         }
 
-        fun Set<NodeAndId<MyNodes>>.hasOnly(testFor: Set<NodeAndId<MyNodes>>): Boolean {
-            return (this.size() == testFor.size() && this.containsAll(testFor))
-        }
 
+        // serialize so we test end to end
         val outputBuffer = ByteArrayOutputStream()
         builder.serialize(outputBuffer)
 
+        // bring graph out of serializatino and use
         useGraph<MyNodes, MyRelations>(ByteArrayInputStream(outputBuffer.toByteArray())) {
-            assertTrue(Actor["Harrison Ford"].manyConnections(StarredIn).hasOnly(setOf(NodeAndId(Movie, "Star Wars"), NodeAndId(Movie, "Indiana Jones"))))
-            assertEquals(NodeAndId(Movie, "Star Wars"), Actor["Mark Hamill"].oneConnection(StarredIn))
-            assertEquals(NodeAndId(Movie, "Star Wars"), Actor["Carrie Fisher"].oneConnection(StarredIn))
-            assertEquals(NodeAndId(Movie, "Star Wars"), Actor["Peter Mayhew"].oneConnection(StarredIn))
-            assertTrue(Movie["Star Wars"].manyConnections(Starring).hasOnly(setOf(NodeAndId(Actor, "Harrison Ford"),
-                    NodeAndId(Actor, "Mark Hamill"),
-                    NodeAndId(Actor, "Carrie Fisher"),
-                    NodeAndId(Actor, "Peter Mayhew"))))
-            assertEquals(NodeAndId(Director, "George Lucas"), Movie["Star Wars"].oneConnection(DirectedBy))
-            assertEquals(NodeAndId(Movie, "Star Wars"), Director["George Lucas"].oneConnection(Directed))
+            assertTrue(Actor["Harrison Ford"].getConnections(StarredIn).hasOnly(setOf(Movie("Star Wars"), Movie("Indiana Jones"))))
+            assertEquals(Movie("Star Wars"), Actor["Mark Hamill"].getSingleConnection(StarredIn))
+            assertEquals(Movie("Star Wars"), Actor["Carrie Fisher"].getSingleConnection(StarredIn))
+            assertEquals(Movie("Star Wars"), Actor["Peter Mayhew"].getSingleConnection(StarredIn))
+            assertTrue(Movie["Star Wars"].getConnections(Starring).hasOnly(setOf(Actor("Harrison Ford"), Actor("Mark Hamill"), Actor("Carrie Fisher"), Actor("Peter Mayhew"))))
+            assertEquals(Director("George Lucas"), Movie["Star Wars"].getSingleConnection(DirectedBy))
+            assertEquals(Movie("Star Wars"), Director["George Lucas"].getSingleConnection(Directed))
 
-            assertEquals(NodeAndId(Director, "Steven Spielberg"), Award["1994 Academy Award for Best Picture"].oneConnection(AwardWinner))
-            assertEquals(NodeAndId(Award, "2005 AFI Life Achievement Award"), Director["George Lucas"].oneConnection(WonAward))
-            assertEquals(NodeAndId(Movie, "Star Wars"), Award["1979 Academy Award for Best Visual Effects"].oneConnection(AwardWinner))
-            assertEquals(NodeAndId(Movie, "Star Wars"), Award["1979 Academy Award for Best Original Music Score"].oneConnection(AwardWinner))
+            assertEquals(Director("Steven Spielberg"), Award["1994 Academy Award for Best Picture"].getSingleConnection(AwardWinner))
+            assertEquals(Award("2005 AFI Life Achievement Award"), Director["George Lucas"].getSingleConnection(WonAward))
+            assertEquals(Movie("Star Wars"), Award["1979 Academy Award for Best Visual Effects"].getSingleConnection(AwardWinner))
+            assertEquals(Movie("Star Wars"), Award["1979 Academy Award for Best Original Music Score"].getSingleConnection(AwardWinner))
 
-            val actualSet = Award["People's Choice Award"].manyConnections(AwardWinner)
-            val expectedSet= setOf(NodeAndId(Movie, "Star Wars"), NodeAndId(Movie, "Indiana Jones"),
-                    NodeAndId(Director, "Steven Spielberg"), NodeAndId(Actor, "Harrison Ford"))
-            assertTrue(actualSet.hasOnly(expectedSet))
+            assertTrue(Award["People's Choice Award"].getConnections(AwardWinner).hasOnly(setOf(Movie("Star Wars"), Movie("Indiana Jones"), Director("Steven Spielberg"), Actor("Harrison Ford"))))
         }
 
     }
