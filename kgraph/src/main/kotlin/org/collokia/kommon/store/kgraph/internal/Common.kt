e616d2cdd4e4008745bb8e9ff88ecc963e0e1c96
jaysonminard <jayson.minard@gmail.com>
2015-06-26 08:47:28 -0300

    Kotlin to 0.12.613 gradle to 2.4 rename vertx modules

---
+++ kgraph/src/main/kotlin/org/collokia/kommon/store/kgraph/internal/Common.kt
@@ -0,0 +1,10 @@
+package org.collokia.kommon.store.kgraph.internal
+
+private data class RelationshipPairKey<N : Enum<N>, R : Enum<R>>(val fromNode: N, val relationship: R)
+private data class RelationshipTrippleKey<N : Enum<N>, R : Enum<R>>(val fromNode: N, val relationship: R, val toNode: N)
+
+private val GRAPH_MARKERS_HEADER = "**KOTLIN-NFGRAPH**"
+private val GRAPH_MARKERS_SCHEMA_HEADER = "**SCHEMA**"
+private val GRAPH_MARKERS_ORDINAL_HEADER = "**ORDINALS**"
+private val GRAPH_MARKERS_GRAPH_HEADER = "**GRAPH**"
+
