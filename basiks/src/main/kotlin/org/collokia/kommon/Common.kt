a43ac86d3733d65fdd0e22e50015dc33059b4cf2
jaysonminard <jayson.minard@gmail.com>
2015-08-21 09:48:44 -0300

    removing things from Kommon now found in Klutter

--- basiks/src/main/kotlin/org/collokia/kommon/Common.kt
+++ basiks/src/main/kotlin/org/collokia/kommon/Common.kt
@@ -1,15 +1,5 @@
 package org.collokia.kommon
 
-public fun <T> T.verifiedBy(verifyWith: (T) -> Unit): T {
-    verifyWith(this)
-    return this
-}
-
-public fun <T> T.initializedBy(initWith: (T) -> Unit): T {
-    initWith(this)
-    return this
-}
-
 public fun <T> T.then(initWith: (T) -> Unit): T {
     initWith(this)
     return this
--- basiks/src/main/kotlin/org/collokia/kommon/jdk.collections.kt
+++
@@ -1,4 +0,0 @@
-package org.collokia.kommon.jdk.collections
-
-// M12 has this:
-// public fun <K,V> Map<K,V>.isNotEmpty(): Boolean = !this.isEmpty()
